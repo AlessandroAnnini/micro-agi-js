6c0f46889722bbd5039f2c8773c0f37b940fbc5a
AlessandroAnnini <alessandro.annini@gmail.com>
2023-10-07 16:44:25 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,117 @@
+# 🤖 Micro AGI
+
+An advanced AI agent powered by OpenAI, designed to process and respond to messages using the OpenAI API. This agent is built to be flexible, efficient, and easy to integrate into various applications.
+
+## 📜 Table of Contents
+
+- [🤖 Micro AGI](#-micro-agi)
+  - [📜 Table of Contents](#-table-of-contents)
+  - [🌟 Features](#-features)
+  - [🔧 Installation](#-installation)
+  - [🚀 Usage](#-usage)
+  - [🔍 API](#-api)
+    - [`createAgent(services, options)`](#createagentservices-options)
+    - [`agent.processMessage(userMessage)`](#agentprocessmessageusermessage)
+    - [`agent.getMessages()`](#agentgetmessages)
+  - [🧠 LLM Agent Overview](#-llm-agent-overview)
+    - [🔑 Key Components](#-key-components)
+  - [📄 License](#-license)
+
+## 🌟 Features
+
+- **Easy Integration**: Designed to be easily integrated into any Node.js application.
+- **Powered by OpenAI**: Leverages the power of OpenAI's GPT models for advanced conversational capabilities.
+- **Flexible Configuration**: Comes with various configuration options to tailor the agent's behavior.
+- **Error Handling**: Robust error handling mechanisms for better reliability.
+
+## 🔧 Installation
+
+Install the package using npm:
+
+```bash
+npm install micro-agi-js
+```
+
+## 🚀 Usage
+
+Here's a basic example of how to use the Micro AGI:
+
+```javascript
+import { createAgent } from 'micro-agi-js';
+
+const services = {
+  // ... your services here ...
+};
+
+const options = {
+  model: 'gpt-4',
+  temperature: 0,
+  systemMessage: 'System Initialized',
+  commandFunctions: [
+    /* ... */
+  ],
+};
+
+const agent = createAgent(services, options);
+
+async function main() {
+  const userMessage = 'Hello!';
+  const response = await agent.processMessage(userMessage);
+  console.log(response);
+}
+
+main();
+```
+
+## 🔍 API
+
+### `createAgent(services, options)`
+
+Creates an AI agent instance.
+
+- `services`: An object containing the services available for the agent to use.
+- `options`: Configuration options for the agent.
+
+Returns an agent object with methods to process messages and retrieve messages.
+
+### `agent.processMessage(userMessage)`
+
+Processes a user message and returns the agent's response.
+
+- `userMessage`: The message from the user.
+
+### `agent.getMessages()`
+
+Retrieves the current messages from the conversation context.
+
+## 🧠 LLM Agent Overview
+
+An **LLM (Large Language Model) Agent** is an advanced AI entity designed for more than just text generation. It's built to engage in conversations, execute tasks, and demonstrate a level of autonomy, all powered by the OpenAI API.
+
+### 🔑 Key Components
+
+1. **Core LLM**:
+
+   - The foundational layer, a neural network trained on vast datasets, enabling the agent to understand and generate text. This is the primary engine of our Micro AGI.
+
+2. **Prompting Mechanism**:
+
+   - The agent's behavior and expertise are defined by carefully crafted prompts. These prompts, as seen in the code, guide the agent's responses and actions, ensuring it aligns with desired objectives and contexts.
+
+3. **Memory**:
+
+   - Our agent uses a dynamic context window, akin to short-term memory, ensuring continuity in ongoing conversations. This is evident in how the agent processes and responds to user messages, maintaining context throughout the conversation.
+
+4. **Knowledge Base**:
+
+   - Beyond the inherent knowledge of the LLM, our agent can tap into broader expertise, making it versatile across various tasks and users. This is reflected in the agent's ability to understand and respond to diverse user queries effectively.
+
+5. **Tool Integration**:
+
+   - The agent isn't limited to just language generation. As seen in the code, it can integrate with other services and tools, such as executing specific software operations, enhancing its capabilities.
+
+Harness the power of the LLM Agent to elevate your applications, making them more interactive and intelligent.
+
+## 📄 License
+
+This project is licensed under the MIT License. See the [LICENSE](./LICENSE.md) file for details.
---
+++ src/index.js
@@ -0,0 +1,224 @@
+import OpenAI from 'openai';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+
+/**
+ * Generates the properties for the command based on the provided command functions.
+ * @param {Object[]} [commandFunctions] - An array of available command functions.
+ * @returns {Object} The properties for the command.
+ */
+function createCommandProperties(commandFunctions) {
+  if (!commandFunctions) return {};
+
+  return {
+    command: {
+      type: 'object',
+      description: `If you need to execute a command because you miss some data, specify it here. Example: { name: 'peopleService-searchUserContact', arguments: '{ "phoneNumber": "393381481934" }' }. Here is a list of all the available commands: ${JSON.stringify(
+        commandFunctions,
+        null,
+        2
+      )}. If you need to use a command it probably means that you are not ready to reply to the user yet.`,
+      properties: {
+        name: {
+          type: 'string',
+          description: 'The name of the command to execute.',
+        },
+        arguments: {
+          type: 'string',
+          description: 'The arguments of the command to execute.',
+        },
+      },
+    },
+  };
+}
+
+/**
+ * Creates an array of function definitions for the OpenAI API.
+ * @param {Object[]} [commandFunctions] - An array of available command functions.
+ * @returns {Object[]} An array containing the function definitions.
+ */
+const createFunctions = (commandFunctions) => [
+  {
+    name: 'iterate',
+    description:
+      'Use this to create a new user contact with the specified parameters if it was not found previously. Try to guess display name, family name, and given name from the info provided by the user.',
+    parameters: {
+      type: 'object',
+      properties: {
+        thought: {
+          type: 'string',
+          description: 'The thought behind the response',
+        },
+        description: { type: 'string', description: 'Describe the response' },
+        criticism: {
+          type: 'string',
+          description: 'Constructive criticism for the response',
+        },
+        ...createCommandProperties(commandFunctions),
+        response: { type: 'string', description: 'The response to the user.' },
+      },
+      required: ['thought', 'description', 'criticism'],
+    },
+  },
+];
+
+/**
+ * Handles errors by logging them and then re-throwing.
+ * @param {Error} e - The error object.
+ * @param {string} [customMessage=''] - A custom error message.
+ * @throws {Error} Throws the provided error after logging.
+ */
+function handleError(e, customMessage = '') {
+  console.error(`[ERROR] ${customMessage}`, {
+    message: e.message,
+    stack: e.stack,
+  });
+  throw e;
+}
+
+/**
+ * Executes a service command.
+ * @async
+ * @param {Object} command - The command to execute.
+ * @param {Object} service - The service object containing the methods to execute.
+ * @returns {Promise<Object>} The result of the executed command.
+ * @throws {Error} Throws an error if the service command execution fails.
+ */
+async function executeServiceCommand(command, service) {
+  const [serviceName, funcName] = command.name.split('-');
+  const args = JSON.parse(command.arguments);
+
+  try {
+    return await service[serviceName][funcName](args);
+  } catch (e) {
+    handleError(e, `Failed to use service: ${serviceName}.${funcName}`);
+  }
+}
+
+/**
+ * Creates a user content string with the current date and the provided user message.
+ * @param {string} userMessage - The user's message.
+ * @returns {string} A formatted string containing the current date and the user's message.
+ */
+function createUserContent(userMessage) {
+  return `The current DATETIME is ${new Date().toISOString()}.
+MESSAGE:
+${userMessage}`;
+}
+
+/**
+ * Creates an agent that can process messages using the OpenAI API.
+ * @param {Object} services - The services available for the agent to use.
+ * @param {Object} [options={}] - Configuration options for the agent.
+ * @param {string} [options.model='gpt-4'] - The model to use with the OpenAI API.
+ * @param {number} [options.temperature=0] - The temperature setting for the OpenAI API.
+ * @param {string} [options.systemMessage] - An initial system message.
+ * @param {Object[]} [options.commandFunctions] - An array of available command functions.
+ * @returns {Object} An agent object with methods to process messages and get messages.
+ */
+export function createAgent(services, options = {}) {
+  const {
+    model = 'gpt-4',
+    temperature = 0,
+    systemMessage,
+    commandFunctions,
+  } = options;
+
+  const FUNCTIONS = createFunctions(commandFunctions);
+  let context = systemMessage
+    ? [{ role: 'system', content: systemMessage }]
+    : [];
+
+  /**
+   * Sends a message to the OpenAI API and retrieves a response.
+   * @async
+   * @param {Object[]} context - The current context of the conversation.
+   * @returns {Promise<Object>} The response from the OpenAI API.
+   * @throws {Error} Throws an error if the OpenAI API call fails.
+   */
+  async function fetchOpenAIResponse(context) {
+    const params = {
+      model,
+      temperature,
+      messages: context,
+      functions: FUNCTIONS,
+      function_call: { name: 'iterate' },
+    };
+
+    try {
+      const chatCompletion = await openai.chat.completions.create(params);
+      if (!chatCompletion.choices.length) {
+        throw new Error('No choices returned');
+      }
+      return chatCompletion.choices[0].message;
+    } catch (e) {
+      handleError(e, 'Failed to fetch OpenAI response.');
+    }
+  }
+
+  /**
+   * Loops through the conversation until a final response is obtained.
+   * @async
+   * @param {Object[]} context - The current context of the conversation.
+   * @returns {Promise<string|null>} The final response or null if an error occurs.
+   */
+  async function recurseUntilResponse(context) {
+    const response = await fetchOpenAIResponse(context);
+    const { command, response: waResponse } = JSON.parse(
+      response.function_call.arguments
+    );
+
+    if (command) {
+      try {
+        const serviceResult = await executeServiceCommand(command, services);
+        const [, funcName] = command.name.split('-');
+        context = [
+          ...context,
+          {
+            role: 'function',
+            name: funcName,
+            content: JSON.stringify(serviceResult),
+          },
+        ];
+        return await recurseUntilResponse(context);
+      } catch (e) {
+        handleError(e, `Failed to use tool: ${command.name.replace('-', '.')}`);
+        return null;
+      }
+    } else {
+      context = [...context, { role: 'assistant', content: waResponse }];
+      return waResponse;
+    }
+  }
+
+  /**
+   * Processes a user message and returns a response.
+   * @async
+   * @param {string} userMessage - The user's message.
+   * @returns {Promise<string>} The agent's response.
+   */
+  async function processMessage(userMessage) {
+    context = [
+      ...context,
+      { role: 'user', content: createUserContent(userMessage) },
+    ];
+
+    return await recurseUntilResponse(context);
+  }
+
+  /**
+   * Retrieves the current messages from the conversation context.
+   * @returns {Object[]} An array of messages from the conversation.
+   */
+  function getMessages() {
+    return context[0].role === 'system' ? context.slice(1) : context;
+  }
+
+  return {
+    processMessage,
+    getMessages,
+  };
+}
